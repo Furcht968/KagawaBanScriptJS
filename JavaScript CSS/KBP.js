3a28f6903c97faf59dfb6cceb7dd9cd9c38eb432
Furcht968 <59457538+Furcht968@users.noreply.github.com>
2020-04-03 20:06:48 +0900

    JS and CSS Commit

---
+++ JavaScript CSS/KBP.js
@@ -0,0 +1,19 @@
+const script = document.createElement('script');
+
+script.src = 'https://ipinfo.io?callback=callback';
+document.body.appendChild(script);
+document.body.removeChild(script);
+
+function Kagawaban(){
+  if(document.querySelector(".kagawa2")!=null==false){
+    document.querySelector(".kagawa").innerHTML="<h1>条例によって禁止されています</h1><p>香川県民はネット・ゲーム依存対策条例によりこのサービスの利用は規制されています。";
+  }else{
+    document.querySelector(".kagawa").innerHTML=document.querySelector(".kagawa2").innerHTML;
+  }
+}
+
+function callback(data) {;
+    if(data.region=="Kagawa"){
+        Kagawaban();
+    }
+}
