bf5743ebffb4418582767ccea2afba023e7fdf4e
Furcht968 <59457538+Furcht968@users.noreply.github.com>
2020-04-03 20:25:29 +0900

    Update to 1.1

--- JavaScript CSS/KBP.js
+++ JavaScript CSS/KBP.js
@@ -6,9 +6,9 @@ document.body.removeChild(script);
 
 function Kagawaban(){
   if(document.querySelector(".kagawa2")!=null==false){
-    document.querySelector(".kagawa").innerHTML="<h1>条例によって禁止されています</h1><p>香川県民はネット・ゲーム依存対策条例によりこのサービスの利用は規制されています。";
+    document.querySelector(".kagawa").innerHTML="<h1>条例によって禁止されています</h1><p>香川県民はネット・ゲーム依存対策条例によりこのサービスの利用は規制されています。</p>";
   }else{
-    document.querySelector(".kagawa").innerHTML=document.querySelector(".kagawa2").innerHTML;
+	document.querySelector(".kagawa").innerHTML=document.querySelector(".kagawa2").innerHTML;
   }
 }
 
